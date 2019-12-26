7214999a1ff7f8bf234f4857410480337633589e
INNA YEDZINOVICH <innayedzinovich@gmail.com>
2019-12-26 16:55:22 -0500

    connection established between files

---
+++ app.js
@@ -0,0 +1,15 @@
+//modules
+const http = require('http');
+
+//file imports
+const respond = require('./lib/respond.js');
+
+//connections
+const port = process.env.port || 3000; 
+
+
+//Create server
+const server = http.createServer(respond);
+server.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
