26521891609cdecf915df04d4934a2ace3702384
INNA YEDZINOVICH <innayedzinovich@gmail.com>
2020-01-03 23:27:33 -0500

    pre-deployment

--- static/project_files/script.js
+++ static/project_files/script.js
@@ -1,80 +1,93 @@
+//loop through children of tbody
 const children = $('tbody').children();
+console.log(children.forEach);
 
-var children_arr = [];
-for(var i = 0; i < children.length; i++){
-    children_arr.push(children[i]);
+//convert children to an array
+let children_array = [];
+for(let i = 0; i < children.length; i++){
+    children_array.push(children[i]);
 }
+console.log(children_array);
 
+//build an array of objects
 const items = [];
-children_arr.forEach(element => {
+children_array.forEach(element => {
+    console.log(element.outerHTML);
+    console.log(element.getAttribute('data-name'));
     const rowDetails = {
-    name: element.getAttribute('date-name'), 
-    size: parseInt(element.getAttribute('date-size')), 
-    time: parseInt(element.getAttribute('date-time')), 
-    html: element.outerHTML
+        name: element.getAttribute('data-name'),
+        size: parseInt(element.getAttribute('data-size')),
+        time: parseInt(element.getAttribute('data-time')),
+        html: element.outerHTML
     };
     items.push(rowDetails);
 });
+console.log(items);
 
-//Sort Status
+//Sort status
 const sortStatus = {
-    name: 'none', 
-    size: 'none', 
-    time: 'none'
+    name: 'none', //none, up, down
+    size: 'none', //none, up, down
+    time: 'none' //none, up, down
 };
 
-const sort_up = (items, type) => {
-  items.sort((obj1, obj2) => {
-      var val1, val2;
-      
-      if(type === 'name'){
-        val1 = obj1.name.toLowerCase();
-        val2 = obj2.name.toLowerCase();
-      }
-      else if(type === 'size'){
-        val1 = obj1.size;
-        val2 = obj2.size;    
-      }
-      else {
-        val1 = obj1.time;
-        val2 = obj2.time;
-      }
-      
-      if(val1 < val2)
-        return -1;
-      if(val1 > val2)
-        return 1;
-      return 0;
-  });  
-};
+const sort = (items, option, type) => {
+    items.sort((item1, item2) => {
+        let value1, value2;
+        if(type === 'name'){
+            value1 = item1.name.toUpperCase();
+            value2 = item2.name.toUpperCase();
+        }else if(type === 'size'){
+            value1 = item1.size;
+            value2 = item2.size;
+        }else{
+            value1 = item1.time;
+            value2 = item2.time;
+        }
+        if(value1 < value2){
+            return -1;
+        }
+        if(value1 > value2){
+            return 1;
+        }
+        //equal values
+        return 0;
+    });
 
-const sort_down = (items, type) =>{
-    sort_up(items,type);
-    items.reverse();
+    //reverse the array if the option is down
+    if(option === 'down'){
+        items.reverse();
+    }
 };
 
 //fill table body with items
-const fill_table_body = (items) =>{
-    const content = items.map(element => element.html.join());
+const fill_table_body = items => {
+    const content = items.map(element => element.html).join('');
+    console.log(content);
     $('tbody').html(content);
 };
 
-//events listeners
-document.getElementById('table_head_row').addEventListener('click', event => {
-    if(event.target) {
+//event listeners
+document.getElementById('table_head_row').addEventListener('click', event =>{
+    if(event.target){
+        //clear icons
         $('ion-icon').remove();
-            
-        if(sortStatus[event.target.id] in ['none', 'down']){
-            sort_up(items, event.target.id );
+
+        if(['none', 'down'].includes(sortStatus[event.target.id])){
+            //sort in ascending order
+            sort(items, 'up', event.target.id);
             sortStatus[event.target.id] = 'up';
-                
+            //add icon
             event.target.innerHTML += ' <ion-icon name="arrow-dropup-circle"></ion-icon>';
         }
         else if(sortStatus[event.target.id] === 'up'){
-            sort_down(items, event.target.id);
+            //sort in descending order
+            sort(items, 'down', event.target.id);
             sortStatus[event.target.id] = 'down';
-            event.target.innerHTML += ' <ion-icon name="arrow-dropdown-circle"></ion-icon>'
+            //add icon
+            event.target.innerHTML += ' <ion-icon name="arrow-dropdown-circle"></ion-icon>';
         }
         fill_table_body(items);
     }
 });
+
